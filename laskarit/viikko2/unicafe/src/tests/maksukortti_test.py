ff14a7f3cf9fd84b57ccd9499a922dcc9a53fb79
Henri Hautamäki <henri.juho.hautamaki@gmail.com>
2023-03-26 13:30:39 +0300

    Laskaritehtäviä jatkettu

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_kortin_saldo_kasvaa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+
+    def test_rahan_ottaminen_toimii_oikein_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_rahan_ottaminen_toimii_oikein_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(1001)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_ottaminen_toimii_oikein_jos_rahaa_tarpeeksi_boolean(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+    def test_rahan_ottaminen_toimii_oikein_jos_rahaa_ei_tarpeeksi_boolean(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1001), False)
